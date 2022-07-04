2b4fd19c424fce635d5e58707d18c49657730cb3
SimoneHell <simone.hall.speaks@gmail.com>
2022-07-04 19:02:12 +0000

    Card component almost finished

--- src/js/component/cardcomponent-body.jsx
+++ src/js/component/cardcomponent-body.jsx
@@ -2,13 +2,13 @@ import React from "react";
 //removed baby rigo image
 
 //created card component
-const CardComponent = {
-	<div className="card">
-  <img src="..." className="card-img-top" alt="...">
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-    <a href="#" className="btn btn-primary">Find Out More!</a>
+const CardComponent = (props) =>
+{return <div className="card">
+  <img src="https://via.placeholder.com/500x325" className="card-img-top" alt="..."/>
+	<div className="card-body">
+    <h5 className="card-title">{props.cardTitle}</h5>
+    <p className="card-text">{props.cardText}</p>
+    <a href="#" className="btn btn-primary">{props.cardButton}</a>
   </div>
 </div>
 }
@@ -16,4 +16,4 @@ const CardComponent = {
 
 	
 
-export default Home;
+export default CardComponent;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import CardComponent from "./component/cardcomponent-body.jsx"
+import CardComponent from "./cardcomponent-body.jsx"
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return ( <><CardComponent/>
+	return ( <><CardComponent cardTitle={"Card Title"} cardText={"This is an example of test Ill use for the first card."} cardButton={"Find Out More!"}/>
+	<CardComponent cardTitle={"Card Title"} cardText={"In the next card I'll use some other words like Lorem Ipsum, again and again and again."} cardButton={"Find Out More!"}/>
+	<CardComponent cardTitle={"Card Title"} cardText={"Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"} cardButton={"Find Out More!"}/>
+	<CardComponent cardTitle={"Card Title"} cardText={"This is the last card where I'll add any kind of sentences. I hope I can do the next part correctly."} cardButton={"Find Out More!"}/>
 	</>
 		
 	);
