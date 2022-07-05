ee50fd9856f78f8af48485b8f5563635d099f631
SimoneHell <simone.hall.speaks@gmail.com>
2022-07-05 16:35:36 +0000

    Finished Card component FINAL

--- src/js/component/cardcomponent-body.jsx
+++ src/js/component/cardcomponent-body.jsx
@@ -1,13 +1,5 @@
 import React from "react";
 //removed baby rigo image
-const CardContainer = () => {
-	<>
-<CardComponent cardTitle={"Card Title"} cardText={"This is an example of test Ill use for the first card."} cardButton={"Find Out More!"}/>
-<CardComponent cardTitle={"Card Title"} cardText={"In the next card I'll use some other words like Lorem Ipsum, again and again and again."} cardButton={"Find Out More!"}/>
-<CardComponent cardTitle={"Card Title"} cardText={"Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"} cardButton={"Find Out More!"}/>
-<CardComponent cardTitle={"Card Title"} cardText={"This is the last card where I'll add any kind of sentences. I hope I can do the next part correctly."} cardButton={"Find Out More!"}/>
-	</>
-}
 //created card component
 const CardComponent = (props) =>
 {return <div className="card col-3">
@@ -23,4 +15,4 @@ const CardComponent = (props) =>
 
 	
 
-export default CardContainer;
+export default CardComponent;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import CardContainer from "./cardcomponent-body.jsx"
+import CardComponent from "./cardcomponent-body.jsx"
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return  <CardContainer/>
-		
-};
-
+	return (<div className="row d-flex">
+	<CardComponent cardTitle={"Card Title"} cardText={"This is an example of test Ill use for the first card."} cardButton={"Find Out More!"}/>
+	<CardComponent cardTitle={"Card Title"} cardText={"In the next card I'll use some other words like Lorem Ipsum, again and again and again."} cardButton={"Find Out More!"}/>
+	<CardComponent cardTitle={"Card Title"} cardText={"Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"} cardButton={"Find Out More!"}/>
+	<CardComponent cardTitle={"Card Title"} cardText={"This is the last card where I'll add any kind of sentences. I hope I can do the next part correctly."} cardButton={"Find Out More!"}/>
+	</div>)
+}
 export default Home;
